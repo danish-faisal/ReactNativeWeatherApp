81f19150be381115c4e6f1aec08bfd304f0cc349
Danish Faisal <danishfaisal.wwe@gmail.com>
2022-09-16 17:17:24 +0530

    Backgroud image added & axios installed

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("./assets/images/bg3.jpeg")}
+        resizeMode="cover"
+        style={styles.bgImage}
+      ></ImageBackground>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  bgImage: {
     flex: 1,
   },
 });
